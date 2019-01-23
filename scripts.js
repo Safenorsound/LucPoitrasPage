a8f08d01f5f5a64e8fd77ad94349b77ee9a60522
Safenorsound <ijroback@gmail.com>
2019-01-23 10:31:31 -0800

    remove erroneous pic

--- scripts.js
+++ scripts.js
@@ -1,4 +1,15 @@
 $(document).ready(function() {
+  $('#nav').click(function(event) {
+    var headerHeight = $('header').height();
 
+    // event.preventDefault();
 
-}
+    $('html, body').animate(
+      {
+        scrollTop:
+          $($.attr(this, 'href')).offset().top - $('header').height() + 5
+      },
+      500
+    );
+  });
+});
